a0bbb8d48d1c54a87c3065211295343f6b845ae8
Thore Johnsen <thore.johnsen@evry.com>
2018-08-22 11:24:51 +0200

    Frontend SPA now calls web backend using ID token.

--- src/main/java/teedjay/backend/TheBackendApi.java
+++ src/main/java/teedjay/backend/TheBackendApi.java
@@ -1,5 +1,7 @@
 package teedjay.backend;
 
+import io.jsonwebtoken.Claims;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -17,8 +19,9 @@ public class TheBackendApi {
     @GET
     public InfoStructure getSomeInfo(@HeaderParam("Authorization") String authorization) {
         if ((authorization == null) || !authorization.startsWith("Bearer ")) throw new NotAuthorizedException("Missing bearer token");
-        String token = authorization.substring(7);
-        return new InfoStructure(jwtDecoder.getClaimsFromToken(token));
+        String token = authorization.substring(7); // remove "Bearer " prefix
+        Claims claimsFromToken = jwtDecoder.getClaimsFromToken(token);
+        return new InfoStructure(claimsFromToken);
     }
 
 }
